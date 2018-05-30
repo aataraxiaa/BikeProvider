2ffa06447c0cf674607c58364cfd19d5635af4a1
Pete Smith <peadar81@gmail.com>
2018-05-30 14:29:16 +0700

    watchOS support

---
+++ BikeProviderwatchOS/BikeProviderwatchOS.h
@@ -0,0 +1,19 @@
+//
+//  BikeProviderwatchOS.h
+//  BikeProviderwatchOS
+//
+//  Created by Pete Smith on 30/05/2018.
+//  Copyright © 2018 Pete Smith. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for BikeProviderwatchOS.
+FOUNDATION_EXPORT double BikeProviderwatchOSVersionNumber;
+
+//! Project version string for BikeProviderwatchOS.
+FOUNDATION_EXPORT const unsigned char BikeProviderwatchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BikeProviderwatchOS/PublicHeader.h>
+
+
